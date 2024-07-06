90613c9da2c4eff571fd85c8c421a0c14b1820f6
meixiang474 <1436098850@qq.com>
2024-07-06 13:44:30 +0800

    feat: add tailwindcss

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,7 @@
   <RouterLink to="/">home</RouterLink>
   <RouterLink to="/about">about</RouterLink>
   <div>hello world</div>
+  <h1 class="text-3xl font-bold underline">hello tailwind</h1>
 </template>
 
 <script setup lang="ts">
