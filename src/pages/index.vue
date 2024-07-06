9bff6b027abe2ff1370f4b135efd24c66f1d47e4
meixiang474 <1436098850@qq.com>
2024-07-06 10:37:49 +0800

    feat: add unplugin-vue-router

---
+++ src/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <RouterLink to="/">home</RouterLink>
+  <RouterLink to="/about">about</RouterLink>
+  <div>hello world</div>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
